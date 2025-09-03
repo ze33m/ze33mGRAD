da4372acc39fafb2e62b48602fd65d4a9cb38080
Maxim <m4xim.zemskov@gmail.com>
2025-09-03 15:59:57 +0300

    Momentum gd

--- engine/Optims.py
+++ engine/Optims.py
@@ -4,14 +4,25 @@ except:
     from engine.Tensor import Tensor
 
 import numpy as np
-from typing import List
+
 
 class GradientDescent:
-    def __init__(self, params: List[Tensor], lr=1e-3):
+    def __init__(self, params: list[Tensor], lr=1e-3):
         self.params = params
         self.lr = lr
 
     def step(self):
         for weight in self.params:
             weight.data -= self.lr * weight.grad
-        
+
+class MomentumGD:
+    def __init__(self, params: list[Tensor], y, n):
+        self.params = params
+        self.y = y
+        self.n = n
+        self.velocities = [0 for i in range(len(params))]
+
+    def step(self):
+        for i, weight in enumerate(self.params):
+            self.velocities[i] = self.y * self.velocities[i] + self.n * weight.grad
+            weight.data -= self.velocities[i]
